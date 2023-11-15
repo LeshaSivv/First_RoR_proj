a3e8f2b6530b29115da9b4b2f7b73967ce252170
siv <leshasiv2003@gmail.com>
2023-11-15 18:16:26 +0300

    add sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,41 @@ class UsersController < ApplicationController
   end
 
   def create
-    debugger
-    User.create(user_params)
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    else
+      flash.now[:alert] = 'Неправильно заполнены поля!'
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'Вы успешно поменяли данные!'
+    else
+      flash.now[:alert] = 'Неправильно заполнены поля!'
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    session.delete(:user_id)
+    redirect_to root_path, notice: 'Пользователь удален'
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :nickname, :email, :password)
+    params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+
+  before_save :downcase_nickname
+
+  validates :email, presence: true, uniqueness: true
+  validates :nickname, presence: true, length: {in: 3..12}
+  validates :name, presence: true, length: {in: 3..12}
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
